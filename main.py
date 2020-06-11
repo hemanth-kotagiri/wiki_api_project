0f031b01087150fef74aac1210266249847de9cb
Hemanth <hemanth.kotagiri43@gmail.com>
2020-06-11 23:44:53 +0530

    Add quit statement

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import wikipedia as wiki
 
+#Documentation : https://wikipedia.readthedocs.io/en/latest/
+
 # print(wiki.__version__)
 
 # print(wiki.summary("Wikipedia"))
 
 def ask_user():
-    search = input("What do you want me to search today: ").title()
-
+    search = input("What do you want me to search today(q to quit): ").title()
+    if search == 'Q':
+        exit()
     results = wiki.page(search)
     print(results.title)
     print(results.content)
 
 
 if __name__ == "__main__":
-    ask_user()
+    while True:
+        ask_user()
